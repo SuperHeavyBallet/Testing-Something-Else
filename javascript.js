5e7d81a8d28cb3c7e4e8cdcb14fe5684b9276d0f
Alexander Gorham <agram@hotmail.co.uk>
2024-03-15 12:11:29 +0100

    Initial setup

---
+++ javascript.js
@@ -0,0 +1,15 @@
+console.log("Hmm?");
+
+function sayHello()
+{
+    console.log("Hello");
+}
+
+sayHello();
+
+function saySomething(something)
+{
+    console.log(something);
+}
+
+saySomething("Hello?");
